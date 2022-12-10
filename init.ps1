7adb70d03f88e01b2764af5917481d24a4971ee5
JohnLest <j_schwickerath@hotmail.com>
2022-12-10 10:28:10 +0100

    Change order instruction in init file

--- init.ps1
+++ init.ps1
@@ -4,13 +4,13 @@ if (!(Test-Path $registryPath)) {
     Exit
 }
 
+$userAdmin = "$Env:USERDOMAIN\$Env:USERNAME"
+Write-Output "Add UserAdmin $userAdmin in system Variable "
+New-ItemProperty -Path $registryPath -Name UserAdmin -Value "$userAdmin" -PropertyType string -Force | Out-Null
+
 $name = "PATH"
 $varPath = Get-ItemProperty -Path $registryPath -Name $name
 $realVarPath = $varPath.PATH
 $scriptsPath = "$PSScriptRoot\scripts\;"
 Write-Output "Edit the system variable Path with : $scriptsPath"
 New-ItemProperty -Path $registryPath -Name $name -Value "$realVarPath$scriptsPath" -PropertyType string -Force | Out-Null
-
-Write-Output "Add UserAdmin in system Variable "
-$userAdmin = "$Env:USERDOMAIN\$Env:USERNAME"
-New-ItemProperty -Path $registryPath -Name UserAdmin -Value "$userAdmin" -PropertyType string -Force | Out-Null
